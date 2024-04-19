552f4cdbd0bb751664748a28c0daab2612629203
N74Mmerino <nnicolasmmerino@gmail.com>
2024-04-19 20:21:32 -0300

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// En esta linea asigno la variable marca a la variable marca
+// el valor a la variable enviada mientras el metodo get
+ $marca = $_GET ['m'];
+
+
+ $version = $_GET ['v'];
+
+echo 'marca: '.$marca ;
+echo '<br>';
+echo 'version: '.$version ;
